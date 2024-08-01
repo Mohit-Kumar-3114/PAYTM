ed90bc59892a23ea6029c483b8fb40cecf631084
Mohit <mohitahlawat912@gmail.com>
2024-08-01 22:30:29 +0530

    paytm

--- apps/user-app/components/AppbarClient.tsx
+++ apps/user-app/components/AppbarClient.tsx
@@ -1,19 +1,17 @@
-"use client";
-import { signOut } from "next-auth/react";
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
 import { Appbar } from "@repo/ui/appbar";
 import { useRouter } from "next/navigation";
 
 export function AppbarClient() {
+  const session = useSession();
   const router = useRouter();
 
   return (
-    <div>
-      <Appbar
-        onSignout={async () => {
-          await signOut({ callbackUrl: "/api/auth/signin" });
-        }}
-      />
-    </div>
+   <div>
+      <Appbar onSignin={signIn} onSignout={async () => {
+        await signOut({callbackUrl:"api/auth/sigin"})
+      }} user={session.data?.user} />
+   </div>
   );
 }
-
