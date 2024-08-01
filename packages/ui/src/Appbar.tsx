aba9af07b113feedc4b8393c3d5b18c86eaee25f
Mohit <mohitahlawat912@gmail.com>
2024-08-02 00:05:05 +0530

    paytm

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -1,16 +1,10 @@
 import { Button } from "./button";
 
 interface AppbarProps {
-    user?: {
-        name?: string | null;
-    },
-    onSignin: any,
     onSignout: any
 }
 
 export const Appbar = ({
-    user,
-    onSignin,
     onSignout
 }: AppbarProps) => {
     return <div className="flex justify-between border-b px-4 border-slate-300">
@@ -18,7 +12,7 @@ export const Appbar = ({
             PayTM
         </div>
         <div className="flex flex-col justify-center pt-2">
-            <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
+            <Button onClick={onSignout}>Logout</Button>
         </div>
     </div>
 } 
